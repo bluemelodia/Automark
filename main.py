c51df4bf12e27f687bbf171a0f575a37e5905401
Melanie Hsu <hsumelanie@yahoo.com>
2016-05-10 11:05:00 -0400

    Validated watermark and photo.

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import cv2
 import os
 import sys
+import imghdr
 import numpy as np
 from imutils import paths
 from Tkinter import *
 
+images = ['jpeg', 'png']
+
 def main():
+	# Assumed that watermark has transparent background
 	wm = raw_input("Provide Watermark: ")
 	try: 
-		assert os.path.isfile(wm)
-		wm = os.path.abspath(wm)
+		wm_a = os.path.abspath(wm.strip())
+		assert os.path.exists(wm_a)
 	except:
 		sys.exit("Invalid path to watermark.")
+	try:
+		assert imghdr.what(wm_a) in images	
+	except:
+		sys.exit("Supported formats: jpeg, png.")
+
 	p = raw_input("Provide Photo: ")		
 	try:
-		assert os.path.isfile(p)
-		p = os.path.abspath(p)
+		p_a = os.path.abspath(p.strip())
+		assert os.path.exists(p_a)
 	except:
 		sys.exit("Invalid path to photo.")
-	
+	try: 
+		assert imghdr.what(p_a) in images
+	except:
+		sys.exit("Supported formats: jpeg, png.")
+
+	color = askcolor() 	
 	
 
 if __name__ == "__main__": main()
